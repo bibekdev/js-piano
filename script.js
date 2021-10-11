af9fb780c0e7a4ec38f39462d56b5ff75f4b779d
Bibek Chaudhary <kingbibekc@gmail.com>
2021-10-11 21:34:46 +0545

    piano with html and js

---
+++ script.js
@@ -0,0 +1,52 @@
+let keyboard = document.querySelector(".piano__keyboard");
+let pianoNotes = ["C", "D", "E", "F", "G", "A", "B"];
+
+let init = () => {
+  for (let i = 1; i <= 5; i++) {
+    for (let j = 0; j < 7; j++) {
+      let key = createKey("white", pianoNotes[j], i);
+      keyboard.appendChild(key);
+
+      if (j != 2 && j != 6) {
+        key = createKey("black", pianoNotes[j], i);
+        let emptySpace = document.createElement("div");
+        emptySpace.className = "empty-space";
+        emptySpace.appendChild(key);
+        keyboard.appendChild(emptySpace);
+      }
+    }
+  }
+};
+
+let createKey = (type, note, octave) => {
+  let key = document.createElement("button");
+  key.className = `piano__key piano__key--${type}`;
+
+  key.dataset.letterNote =
+    type == "white" ? note + octave : note + "#" + octave;
+
+  key.dataset.letterNoteFileName =
+    type == "white" ? note + octave : note + "s" + octave;
+
+  key.textContent = key.dataset.letterNote;
+
+  key.addEventListener("mousedown", () => {
+    playSound(key);
+    key.classList.add("piano__key--playing");
+  });
+  key.addEventListener("mouseup", () => {
+    key.classList.remove("piano__key--playing");
+  });
+  key.addEventListener("mouseleave", () => {
+    key.classList.remove("piano__key--playing");
+  });
+  return key;
+};
+
+let playSound = (key) => {
+  let audio = document.createElement("audio");
+  audio.src = "sounds/" + key.dataset.letterNoteFileName + ".mp3";
+  audio.play().then(() => audio.remove());
+};
+
+init();
